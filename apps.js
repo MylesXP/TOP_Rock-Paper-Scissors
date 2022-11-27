23fcf00d1c82d42b5cc30e54a32598508eea87c0
MylesXP <mylesparkinson12@gmail.com>
2022-11-27 15:27:47 -0800

    Initial commit
    Added some pseudo code to guide me and the basic skeleton for some inputs and functions

---
+++ apps.js
@@ -0,0 +1,10 @@
+// Record PLayer Input
+let playerSelection = prompt('Enter your pick:');
+
+// Create function getComputerChoice, which randomly returns rock, paper or scissors.
+function getComputerChoice() {
+    let randomChoice = 
+}
+
+// Create function singleRound that plays a single round of Rock, paper scissors. Should take ...
+// ... two parameters: playerSelection and computerSelection then returns a string declaring winner
