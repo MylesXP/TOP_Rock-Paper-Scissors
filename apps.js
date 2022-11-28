c06792aedea9177ef3b27e9315cf6b5c9fc9eb1d
MylesXP <mylesparkinson12@gmail.com>
2022-11-28 13:27:10 -0800

    Created game() for playing 5 round games
    Created game() along with it's accompanying logic to create a loop to play the game 5 times and then display a result, calling singleRound() inside to start and continue the loop.

--- apps.js
+++ apps.js
@@ -1,21 +1,24 @@
 // Record and compare player input for desired player output
-let getPlayerSelection = prompt('Enter your choice:', "");
-let playerSelection;
+// let getPlayerSelection = prompt('Enter your choice:', "");
+// let playerSelection;
 
-if (!getPlayerSelection) {
-    playerSelection = 'Please enter rock, paper or scissors'
-}  else if (getPlayerSelection.toUpperCase() === 'SCISSORS') {
-    playerSelection = 'SCISSORS'
-} else if (getPlayerSelection.toUpperCase() === 'PAPER') {
-    playerSelection = 'PAPER'
-} else if (getPlayerSelection.toUpperCase() === 'ROCK') {
-    playerSelection = 'ROCK'
-} 
-else {
-    playerSelection = 'Please enter rock, paper or scissors'
-};
+// if (!getPlayerSelection) {
+//     playerSelection = 'Please enter rock, paper or scissors'
+// }  else if (getPlayerSelection.toUpperCase() === 'SCISSORS') {
+//     playerSelection = 'SCISSORS'
+// } else if (getPlayerSelection.toUpperCase() === 'PAPER') {
+//     playerSelection = 'PAPER'
+// } else if (getPlayerSelection.toUpperCase() === 'ROCK') {
+//     playerSelection = 'ROCK'
+// } 
+// else {
+//     playerSelection = 'Please enter rock, paper or scissors'
+// };
 
-console.log('Player: ' + playerSelection);
+// console.log('Player: ' + playerSelection);
+
+let compScore = 0;
+let playerScore = 0;
 
 // Create function getComputerChoice, which randomly returns rock, paper or scissors.
  function getComputerChoice() {
@@ -46,28 +49,77 @@ console.log('Player: ' + playerSelection);
 
 function singleRound(playerSelection, computerSelection){
     let roundResult;
+    
     if (computerSelection === playerSelection) {
        roundResult = 'Draw';
     } else if (computerSelection === 'ROCK' && playerSelection === 'PAPER') {
         roundResult = 'Player won; PAPER beats ROCK';
+        playerScore = ++playerScore
+        console.log('Player Score: ' + playerScore)
     } else if (computerSelection === 'PAPER' && playerSelection === 'ROCK') {
         roundResult = 'Computer won; PAPER beats ROCK';
+        compScore = ++compScore
+        console.log('Computer Score: ' + compScore)
     } else if (computerSelection === 'PAPER' && playerSelection === 'SCISSORS') {
         roundResult = 'Player won; SCISSORS beats PAPER';
+        playerScore = ++playerScore
+        console.log('Player Score: ' + playerScore)
     } else if (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') {
         roundResult = 'Computer won; SCISSORS beats PAPER';
+        compScore = ++compScore
+        console.log('Computer Score: ' + compScore)
     } else if (computerSelection === 'SCISSORS' && playerSelection === 'ROCK') {
         roundResult = 'Player won; ROCK beats SCISSORS';
+        playerScore = ++playerScore
+        console.log('Player Score: ' + playerScore)
     } else if (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') {
         roundResult = 'Computer won; ROCK beats SCISSORS';
+        compScore = ++compScore
+        console.log('Computer Score: ' + compScore)
     } else {
         roundResult = 'ERROR';
     }
-    console.log('Result: ' + roundResult);
+    console.log('Match Result: ' + roundResult);
+    console.log('')
     return roundResult;
 };
 
-singleRound(playerSelection, getComputerChoice());
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let getPlayerSelection = prompt('Enter your choice:', "");
+        let playerSelection;
+
+        if (!getPlayerSelection) {
+            playerSelection = 'Please enter rock, paper or scissors'
+        }  else if (getPlayerSelection.toUpperCase() === 'SCISSORS') {
+            playerSelection = 'SCISSORS'
+        } else if (getPlayerSelection.toUpperCase() === 'PAPER') {
+            playerSelection = 'PAPER'
+        } else if (getPlayerSelection.toUpperCase() === 'ROCK') {
+            playerSelection = 'ROCK'
+        } 
+        else {
+            playerSelection = 'Please enter rock, paper or scissors'
+        };
+
+        console.log('Player: ' + playerSelection);
+        singleRound(playerSelection, getComputerChoice());
+     }
+
+     if (compScore > playerScore) {
+        console.log('Final Result: Computer Wins!');
+    } else if (compScore < playerScore) {
+        console.log('Final Result: Player Wins!');
+    } else if (compScore === playerScore) {
+        console.log('Final Result: A Draw!');
+    } else {
+        console.log('Final Result: ERROR');
+    }
+}
+
+console.log(game())
+
 
 // Write new function called game() that calls the singleRound function inside of it to play
 // a 5 round game that keeps score and reports a winner or loser
