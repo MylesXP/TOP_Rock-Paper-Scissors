ebd0b7e399fc87ba3c543b0c10bbf801fe827ca8
MylesXP <mylesparkinson12@gmail.com>
2022-12-14 18:19:58 -0800

    Rewritten Code for new UI
    Refactored and rewritten HTML, CSS and JS code to work with the new UI buttons.
    Added a visual score counter in HTML doc for player and computer, result box to show context of result.

--- apps.js
+++ apps.js
@@ -2,28 +2,34 @@
 // Declaring score variables to be used in multiple other functions
 let compScore = 0;
 let playerScore = 0;
+let choiceRock = document.getElementById('rock');
+let choicePaper = document.getElementById('paper');
+let choiceScissors = document.getElementById('scissors');
+let resultBox = document.getElementById('results-container');
+let resultBox_Result = document.createElement("h3");
+let resultBox_Score = document.createElement("h4");
+let playerScoreCount = document.getElementById('player-score');
+let compScoreCount = document.getElementById('computer-score');
 
 
-// Create function to get player input
-function getPlayerInput () {
-    let playerChoice = prompt('Enter your choice:', "");
-    let playerInput;
+playerScoreCount.textContent = 0;
+compScoreCount.textContent = 0;
+resultBox.appendChild(resultBox_Result);
+resultBox.appendChild(resultBox_Score);
 
-    if (!playerChoice) {
-        playerInput = 'Please enter rock, paper or scissors'
-    }  else if (playerChoice.toUpperCase() === 'SCISSORS') {
-        playerInput = 'SCISSORS'
-    } else if (playerChoice.toUpperCase() === 'PAPER') {
-        playerInput = 'PAPER'
-    } else if (playerChoice.toUpperCase() === 'ROCK') {
-        playerInput = 'ROCK'
-    }
-    else {
-        playerInput = 'Please enter rock, paper or scissors'
-    };
-    console.log('Player: ' + playerInput);
-    return playerInput
-}
+// Button choice logic
+choiceRock.addEventListener('click', () => {
+    singleRound('ROCK', getComputerChoice())
+});
+
+choicePaper.addEventListener('click', () => {
+    singleRound('PAPER', getComputerChoice())
+})
+
+choiceScissors.addEventListener('click', () => {
+    singleRound('SCISSORS', getComputerChoice())
+})
+
 // Create function getComputerChoice, which randomly returns rock, paper or scissors.
  function getComputerChoice() {
      let randomChoice = Math.floor(Math.random() * 3);
@@ -31,19 +37,15 @@ function getPlayerInput () {
 
      if (randomChoice === 0) {
         compSelection = 'ROCK';
-        console.log('Computer: ' + compSelection);
         return compSelection;
      } else if (randomChoice === 1) {
         compSelection = 'PAPER';
-        console.log('Computer: ' + compSelection);
         return compSelection;
      } else if (randomChoice === 2) {
         compSelection = 'SCISSORS';
-        console.log('Computer: ' + compSelection);
         return compSelection;
      } else {
         compSelection = "ERROR HAS OCCURED"
-        console.log('Computer: ' + compSelection);
         return compSelection;
      };
 };
@@ -58,54 +60,49 @@ function singleRound(playerSelection, computerSelection){
        roundResult = 'Draw';
     } else if (computerSelection === 'ROCK' && playerSelection === 'PAPER') {
         roundResult = 'Player won; PAPER beats ROCK';
-        playerScore = ++playerScore
-        console.log('Player Score: ' + playerScore)
+        playerScore = ++playerScore;
     } else if (computerSelection === 'PAPER' && playerSelection === 'ROCK') {
         roundResult = 'Computer won; PAPER beats ROCK';
-        compScore = ++compScore
-        console.log('Computer Score: ' + compScore)
+        compScore = ++compScore;
     } else if (computerSelection === 'PAPER' && playerSelection === 'SCISSORS') {
         roundResult = 'Player won; SCISSORS beats PAPER';
-        playerScore = ++playerScore
-        console.log('Player Score: ' + playerScore)
+        playerScore = ++playerScore;
     } else if (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') {
         roundResult = 'Computer won; SCISSORS beats PAPER';
-        compScore = ++compScore
-        console.log('Computer Score: ' + compScore)
+        compScore = ++compScore;
     } else if (computerSelection === 'SCISSORS' && playerSelection === 'ROCK') {
         roundResult = 'Player won; ROCK beats SCISSORS';
-        playerScore = ++playerScore
-        console.log('Player Score: ' + playerScore)
+        playerScore = ++playerScore;
     } else if (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') {
         roundResult = 'Computer won; ROCK beats SCISSORS';
-        compScore = ++compScore
-        console.log('Computer Score: ' + compScore)
+        compScore = ++compScore;
     } else {
         roundResult = 'ERROR';
     };
-    console.log('Round Result: ' + roundResult);
-    console.log('');
+    resultBox_Result.textContent = roundResult;
+    playerScoreCount.textContent = playerScore;
+    compScoreCount.textContent = compScore;
     return roundResult;
 };
 
 // Write new function called game() that calls the singleRound function inside of it to play
 // a 5 round game that keeps score and reports a winner or loser
-function game() {
-    for (let i = 0; i < 5; i++) {
-        singleRound(getPlayerInput(), getComputerChoice());
-     };
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         singleRound(getPlayerInput(), getComputerChoice());
+//      };
 
-     if (compScore > playerScore) {
-        console.log('Final Result: Computer Wins!');
-    } else if (compScore < playerScore) {
-        console.log('Final Result: Player Wins!');
-    } else if (compScore === playerScore) {
-        console.log('Final Result: A Draw!');
-    } else {
-        console.log('Final Result: ERROR');
-    };
-};
+//      if (compScore > playerScore) {
+//         console.log('Final Result: Computer Wins!');
+//     } else if (compScore < playerScore) {
+//         console.log('Final Result: Player Wins!');
+//     } else if (compScore === playerScore) {
+//         console.log('Final Result: A Draw!');
+//     } else {
+//         console.log('Final Result: ERROR');
+//     };
+// };
 
-console.log(game());
+// console.log(game());
 
 
