c8f6ee9a858ea35d66f322f449c61b31e7690b25
MylesXP <mylesparkinson12@gmail.com>
2022-12-14 20:22:40 -0800

    Added final result header
    Added a final result header tag to display who won after 5 round game, or in the case of a tie, displays tie. Added a new function that contains logic for final result of the game, added to make singleRound function more readable and make code more semantic.

--- apps.js
+++ apps.js
@@ -2,6 +2,7 @@
 // Declaring score variables to be used in multiple other functions
 let compScore = 0;
 let playerScore = 0;
+let roundCount = 0;
 let choiceRock = document.getElementById('rock');
 let choicePaper = document.getElementById('paper');
 let choiceScissors = document.getElementById('scissors');
@@ -10,8 +11,12 @@ let resultBox_Result = document.createElement("h3");
 let resultBox_Score = document.createElement("h4");
 let playerScoreCount = document.getElementById('player-score');
 let compScoreCount = document.getElementById('computer-score');
+let roundCounter = document.getElementById('round-counter');
+let resultTitle = document.getElementById('result-title');
+let finalResult = document.createElement('h2');
+let roundResult;
 
-
+finalResult.style.color = 'green';
 playerScoreCount.textContent = 0;
 compScoreCount.textContent = 0;
 resultBox.appendChild(resultBox_Result);
@@ -54,55 +59,70 @@ choiceScissors.addEventListener('click', () => {
 // ... two parameters: playerSelection and computerSelection then returns a string declaring winner
 
 function singleRound(playerSelection, computerSelection){
-    let roundResult;
-    
     if (computerSelection === playerSelection) {
-       roundResult = 'Draw';
+       roundResult = 'Draw!';
+       roundCount = ++roundCount;
     } else if (computerSelection === 'ROCK' && playerSelection === 'PAPER') {
         roundResult = 'Player won; PAPER beats ROCK';
         playerScore = ++playerScore;
+        roundCount = ++roundCount;
     } else if (computerSelection === 'PAPER' && playerSelection === 'ROCK') {
         roundResult = 'Computer won; PAPER beats ROCK';
         compScore = ++compScore;
+        roundCount = ++roundCount;
     } else if (computerSelection === 'PAPER' && playerSelection === 'SCISSORS') {
         roundResult = 'Player won; SCISSORS beats PAPER';
         playerScore = ++playerScore;
+        roundCount = ++roundCount;
     } else if (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') {
         roundResult = 'Computer won; SCISSORS beats PAPER';
         compScore = ++compScore;
+        roundCount = ++roundCount;
     } else if (computerSelection === 'SCISSORS' && playerSelection === 'ROCK') {
         roundResult = 'Player won; ROCK beats SCISSORS';
         playerScore = ++playerScore;
+        roundCount = ++roundCount;
     } else if (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') {
         roundResult = 'Computer won; ROCK beats SCISSORS';
         compScore = ++compScore;
+        roundCount = ++roundCount;
     } else {
         roundResult = 'ERROR';
+        roundCount = ++roundCount;
     };
+
+    final();
+
     resultBox_Result.textContent = roundResult;
     playerScoreCount.textContent = playerScore;
     compScoreCount.textContent = compScore;
+    roundCounter.textContent = roundCount;
     return roundResult;
 };
 
-// Write new function called game() that calls the singleRound function inside of it to play
-// a 5 round game that keeps score and reports a winner or loser
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//         singleRound(getPlayerInput(), getComputerChoice());
-//      };
+function final () {
+    // add to singleRound if problem
+    if (roundCount == 5) {
+        roundCounter.textContent = roundCount;
+        playerScoreCount.textContent = playerScore;
+        compScoreCount.textContent = compScore;
+        resultTitle.textContent = 'Final Result:'
+        resultBox.removeChild(resultBox_Result)
+        if (playerScore === compScore) {
+            finalResult.textContent = 'Draw!'
+        } else if (compScore > playerScore) {
+            finalResult.textContent = 'Computer Wins!'
+        } else if (compScore < playerScore) {
+            finalResult.textContent = 'Player Wins!'
+        } else {
+            finalResult.style.color = 'red';
+            finalResult.textContent = 'ERROR'
+        }
+    };
 
-//      if (compScore > playerScore) {
-//         console.log('Final Result: Computer Wins!');
-//     } else if (compScore < playerScore) {
-//         console.log('Final Result: Player Wins!');
-//     } else if (compScore === playerScore) {
-//         console.log('Final Result: A Draw!');
-//     } else {
-//         console.log('Final Result: ERROR');
-//     };
-// };
+    resultBox.appendChild(finalResult);
+}
 
-// console.log(game());
+
 
 
